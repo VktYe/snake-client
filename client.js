fbdf4b7a521027294a7acfc6ed20e01a41d08fe5
VktYe <v.yehorenko@gmail.com>
2025-03-22 00:07:22 -0300

    refactored code. moved connection logic into it's owm module in client file

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require("net"); //used Node "net library"
+
+// establishes a connection with the game server
+const connect = function() {  //used createConnection function from net library
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  conn.on("connect", () => {
+    conn.write("Hello from a new client!");
+  });
+
+  conn.on("error", (error) => {
+    console.log("Connecrtion error: ", error);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn; //connection with the server
+};
+
+module.exports = connect;
