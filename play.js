58c2d73e58b906b7243a552e8a19bce80c6878e6
VktYe <v.yehorenko@gmail.com>
2025-03-21 23:52:26 -0300

    established connection with server

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require("net"); //used Node "net library"
+
+// establishes a connection with the game server
+const connect = function () {  //used createConnection function from net library
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  })
+
+  conn.on("connect", () => {
+    conn.write("Hello from a new client!")//code that does something when a connection is first established
+  })
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn; //connection with the server
+};
+
+console.log("Connecting ...");
+connect();
